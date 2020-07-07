423067528b3d6acbc356d6fd7255fbad87cd4e86
Veronica Celseti <veronicacelseti12@gmail.com>
2020-07-07 17:13:45 +0200

    controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Product;
 class ProductController extends Controller
 {
     public function index()
     {
-        return view('');
+        $all_products = Product::all();
+        dd($all_products);
     }
 }
